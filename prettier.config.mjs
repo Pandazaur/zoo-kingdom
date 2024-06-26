5acda18899c7b52c3673bb6cd096e01d4d745aaa
Alex <alexandreborel1994@gmail.com>
2024-06-26 19:30:18 +0200

    feat: setup prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    printWidth: 120,
+}
+
+export default config
