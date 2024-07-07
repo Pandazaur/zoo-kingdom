2f0b81e8f1b6aa1b8242270e348041441ee25f73
Alexandre BOREL <a.borel@frenchfounders.com>
2024-07-07 10:29:33 +0200

    ui: Change colors

--- frontend/components/shared/header/PremiumAccess.tsx
+++ frontend/components/shared/header/PremiumAccess.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useAccount, useReadContract, useWriteContract } from 'wagmi'
 import { useZooPass, contractMainInfos } from '@/lib/contracts/useZooPass'
 import { formatEther } from 'viem'
@@ -24,6 +24,18 @@ export default function PremiumAccess(props: Props) {
         functionName: "zooPassPrice",
     })
 
+    const premiumWording = useMemo(() => {
+        if (isPremium) {
+            return 'You have access to the premium features'
+        }
+
+        if (zooPassPrice) {
+            return `Unlock premium for ${formatEther(zooPassPrice)} ETH`
+        }
+
+        return ''
+    }, [isPremium, zooPassPrice])
+
     const onGoPremium = () => {
 
         if (isPending || isPremium) {
@@ -36,14 +48,15 @@ export default function PremiumAccess(props: Props) {
             args: [account.address],
             value: zooPassPrice
         })
+        
     }
 
     return (
-        <button className="py-2 px-6 rounded-xl border border-black border-2 text-left bg-white inline-flex items-center gap-4" onClick={onGoPremium}>
+        <button className="py-2 px-6 rounded-xl border border-black border-2 text-left bg-amber-100 inline-flex items-center gap-4" onClick={onGoPremium}>
             <BoltIcon width={32} />
             <div>
                 <h4 className={isPremium ? 'font-medium text-amber-500' : 'font-medium'}>{isPremium ? 'Premium' : 'Become premium'}</h4>
-                <p className="text-xs">{isPremium ? 'You have access to the premium features' : `Unlock premium for ${formatEther(zooPassPrice)} ETH`}</p>
+                <p className="text-xs">{premiumWording}</p>
             </div>
             
         </button>
