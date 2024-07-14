fdee1213141529eab22aa400be0b77acf61fdbb0
Alexandre BOREL <a.borel@frenchfounders.com>
2024-07-14 15:47:43 +0200

    feat: redirection

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            // Basic redirect
+            {
+                source: '/',
+                destination: '/app/animals',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
